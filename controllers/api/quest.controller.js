c23451d659dce9f0760e9c69665ed55bf1969702
maksim kirbaba <maksim.kirbaba@gmail.com>
2018-07-23 23:31:23 +0300

    random questions)

--- controllers/api/quest.controller.js
+++ controllers/api/quest.controller.js
@@ -10,14 +10,40 @@ router.get('/getbynum', getByNum);
 
 module.exports = router;
 
-function getAll(req, res) {
-    mongoClient.connect(config.connectionString, function(err, client){
-        var r = Math.floor(Math.random() * 709);
-        client.db("test").collection("Quest").find({ block: 1, num: {$gt :1} }).skip(r).limit(40).toArray(function(err, quest){
-            res.send(quest)
-            client.close();
-        });
-    });
+async function getAll(req, res) {
+    let quest = [];
+    try {
+        const db = await mongoClient.connect(config.connectionString);
+        let questCont = {blockCount:[],numInBlock:[]};
+        questCont.blockCount = await db.collection("Quest").distinct("block");
+        if(config.questCount==undefined) config.questCount=0;
+        let r = Math.round(config.questCount/Math.max.apply(null, questCont.blockCount));
+        for (let block in questCont.blockCount) {
+            // get num quest in block
+            questCont.numInBlock[block] = await db.collection("Quest").find({ block: questCont.blockCount[block]}).count();  
+            // get random massive quevery
+            let num = questCont.numInBlock[block];
+            questCont[block]=[];
+            for (let i=0; i<r; i++){
+               let seed = Math.floor(Math.random() * num);
+               if (num<r) while (questCont[block].indexOf(seed)!=-1) {
+                    seed = Math.floor(Math.random() * num);
+               }
+               questCont[block].push(seed);
+            };
+            // get quest
+            let cursor = db.collection("Quest").find({block:questCont.blockCount[block], num :{$in : questCont[block]}})
+            for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {
+                quest.push(doc);
+            }
+        }
+        //console.log(questCont)
+        db.close();
+    } catch (error) {
+        console.error(error);
+    }finally{
+        res.send(quest)
+    }   
 }
 
 function getByNum(req, res) {
