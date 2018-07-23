25018cd79869176377593bf2d0614b3dc075b61b
Eugene-im <em.92@mail.ru>
2018-07-23 19:58:03 +0300

    add random

--- controllers/api/quest.controller.js
+++ controllers/api/quest.controller.js
@@ -12,9 +12,8 @@ module.exports = router;
 
 function getAll(req, res) {
     mongoClient.connect(config.connectionString, function(err, client){
-        var n = 709;
-        var r = Math.floor(Math.random() * n);
-        client.db("test").collection("Quest").find({ block: 1, num: {$gt :100} }).limit(40).skip(r).toArray(function(err, quest){
+        var r = Math.floor(Math.random() * 709);
+        client.db("test").collection("Quest").find({ block: 1, num: {$gt :1} }).skip(r).limit(40).toArray(function(err, quest){
             res.send(quest)
             client.close();
         });
