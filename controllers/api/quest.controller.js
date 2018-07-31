03a19a565bae3b73e96f46f3094a1830046e8403
Eugene-im <em.92@mail.ru>
2018-08-01 00:20:19 +0300

    good version

--- controllers/api/quest.controller.js
+++ controllers/api/quest.controller.js
@@ -18,24 +18,29 @@ async function getAll(req, res) {
         let questCont = {blockCount:[],numInBlock:[]};
         questCont.blockCount = await db.collection("Quest").distinct("block");
         if(config.questCount==undefined) config.questCount=0;
-        let r = Math.round(config.questCount/Math.max.apply(null, questCont.blockCount));
+        if(config.mainQuestionBlockCount==undefined) config.mainQuestionBlockCount=questCont.blockCount;
+        let questInBlock = Math.round(config.questCount/config.mainQuestionBlockCount);
         for (let block in questCont.blockCount) {
             // get num quest in block
             questCont.numInBlock[block] = await db.collection("Quest").find({ block: questCont.blockCount[block]}).count();  
             // get random massive quevery
             let num = questCont.numInBlock[block];
+            // bonus quest 1 in block
+            let numInBlock=(block <config.mainQuestionBlockCount) ? questInBlock: 1;
             do {
                 questCont[block]=[];
-                for (let i=0; i<r; i++){
-                   let seed = Math.floor(Math.random() * num);
-                   if (num>r) while (questCont[block].indexOf(seed)!=-1 ||seed == 0 || proba>100) {
-                        seed = Math.floor(Math.random() * num);
-                        proba++;
-                   }
+                for (let i=0; i<numInBlock; i++){
+                    let seed = Math.floor(Math.random() * num);
+                    if (num>questInBlock) {
+                        while (questCont[block].indexOf(seed)!=-1 ||seed == 0 || proba>100) {
+                            seed = Math.floor(Math.random() * num);
+                            proba++;
+                        }
+                    }
                    questCont[block].push(seed);
                 };
                 proba++;
-            } while (await db.collection("Quest").find({block:questCont.blockCount[block], num :{$in : questCont[block]}}).count()<r || proba>100);
+            } while (await db.collection("Quest").find({block:questCont.blockCount[block], num :{$in : questCont[block]}}).count()<numInBlock || proba>100);
 
             // get quest
             let cursor = db.collection("Quest").find({block:questCont.blockCount[block], num :{$in : questCont[block]}})
@@ -43,7 +48,7 @@ async function getAll(req, res) {
                 quest.push(doc);
             }
         }
-        //console.log(questCont,quest.length,proba)
+        console.log(questCont,quest.length,proba)
         db.close();
     } catch (error) {
         console.error(error);
