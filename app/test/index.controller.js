9f6f2fdd97e872cd8c7cafa2cce278135aaffa64
Eugene-im <em.92@mail.ru>
2019-03-11 13:14:16 +0200

    add

--- app/test/index.controller.js
+++ app/test/index.controller.js
@@ -9,17 +9,30 @@
     vm.user = null;
     vm.allUsers = null;
     vm.ansvers = null;
+    vm.allClients = null;
 
     initController();
 
     function initController() {
       // get current user
-      UserService.GetCurrent().then(function(user) {
-        vm.user = user;
-      });
+      // UserService.GetCurrent().then(function(user) {
+      //   vm.user = user;
+      // });
+      getAllClients();
+    }
+
+
+    function getAllClients(){
       UserService.GetAll().then(function(users) {
         vm.allUsers = users;
+        console.log(vm.allUsers)
       });
+    }
+
+    function searchClients(name){
+      UserService.GetByUsername(name).then(function(clients){
+        vm.allClients = clients;
+      })
     }
 
     
