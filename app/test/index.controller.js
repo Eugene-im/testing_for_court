779cbae0645a5963b51e28843e202af7a6a965fc
Eugene-im <em.92@mail.ru>
2019-03-15 01:21:33 +0200

    fix search in line

--- app/test/index.controller.js
+++ app/test/index.controller.js
@@ -16,36 +16,25 @@
     initController();
 
     function initController() {
-      // get current user
-      // UserService.GetCurrent().then(function(user) {
-      //   vm.user = user;
-      // });
       getAllClients();
     }
 
 
     function getAllClients(){
-      //if arguments.length == 0 then 
       UserService.GetAllClients().then(function(clients) {
         vm.allClients = clients;
         console.log(vm.allClients);
       });
-      //else 
-      // UserService.GetAllClients().then(function(clients) {
-      //   vm.allClients = clients;
-      //   console.log(vm.allClients);
-      // });
-
     }
 
     function searchClients(){
       var name = vm.name;
-      console.log(vm.name);
+      if (name === "") {getAllClients()}
+      else{
       UserService.GetByClientname(name).then(function(clients){
         vm.allClients = clients;
-        console.log(vm.allClients);
-        // console.log(vm.name);        
-      })
+        })
+      }
     }
 
     
