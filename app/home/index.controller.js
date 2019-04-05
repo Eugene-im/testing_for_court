e2788b82a8b4fd495dacf63771c858cb9d464cfa
Eugene-im <em.92@mail.ru>
2019-04-05 15:38:32 +0300

    fix cam online

--- app/home/index.controller.js
+++ app/home/index.controller.js
@@ -12,6 +12,7 @@
         vm.user = null;
         vm.newUser = null;
         vm.addNew = addNew;
+        vm.snap = snap;
         vm.one = null;
         vm.two = null;
         vm.qwe = null;
@@ -27,6 +28,36 @@
             UserService.GetCurrent().then(function (user) {
                 vm.user = user;
             });
+            // var context = canvas.getContext('2d');
+            var video = document.getElementById('video');
+            var mediaConfig = {
+              video: true
+            };
+            var errBack = function (e) {
+              console.log('An error has occurred!', e)
+            };
+
+            if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+              navigator.mediaDevices.getUserMedia(mediaConfig).then(function (stream) {
+                video.srcObject = stream;
+                video.play();
+              });
+            } else if (navigator.getUserMedia) { // Standard
+              navigator.getUserMedia(mediaConfig, function (stream) {
+                video.src = stream;
+                video.play();
+              }, errBack);
+            } else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
+              navigator.webkitGetUserMedia(mediaConfig, function (stream) {
+                video.src = window.webkitURL.createObjectURL(stream);
+                video.play();
+              }, errBack);
+            } else if (navigator.mozGetUserMedia) { // Mozilla-prefixed
+              navigator.mozGetUserMedia(mediaConfig, function (stream) {
+                video.src = window.URL.createObjectURL(stream);
+                video.play();
+              }, errBack);
+            }
         }
 
         $scope.fileNameChanged = function() {
@@ -47,6 +78,18 @@
           }
 
         }
+
+        function snap(){
+          var canvas = document.getElementById('canvas');
+          function convertCanvasToImage(canvas) {
+            vm.one = canvas.toDataURL("image/png");
+          }
+          document.getElementById('snap').addEventListener('click', function () {
+            // context.drawImage(video, 0, 0, 640, 480);
+            convertCanvasToImage(canvas);
+          });
+        }
+
 
         function addNew() {
           vm.newUser.idfoto1 = vm.one;
